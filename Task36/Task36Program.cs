95e50bfe0398cfc70c39643d12e0f9c79e19a2f3
Ekaterina Biyushkina <biyushkina.k@gmail.com>
2022-12-20 15:32:27 +0300

    Program is ready

---
+++ Task36/Task36Program.cs
@@ -0,0 +1,35 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+int FindSumEvenPosition(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i = i +2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+
+
+
+
+int[] NewArray(int length, int start, int end)
+{
+    int[] array  = new int[length];
+      for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(start, end);
+    }
+return array;
+}
+
+
+//Задать массив из случайных чисел.
+int[] myarray = NewArray(20, 0, 10);
+Console.WriteLine(String.Join(",", myarray));
+//Найти сумму элементов, стоящих на четных позициях
+
+int sumEven = FindSumEvenPosition(myarray); 
+
+//Вывести результат
+Console.WriteLine($"Cумма элементов массива на четных позициях равна {sumEven}");
